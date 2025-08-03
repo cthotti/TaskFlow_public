2318e9b6cbc474a3dc237ea8b48936cba6fadb43
cthotti <cthotti@gmail.com>
2025-08-03 12:18:45 +0530

    python test

--- ml_analyzer/app.py
+++ ml_analyzer/app.py
@@ -8,7 +8,7 @@ app = FastAPI()
 async def analyze(request: Request): 
     try: 
         data = await request.json()
-        return {"message": "Hello World"}
+        return {"message": "hello sexy man 123"}
     except Exception: 
         return JSONResponse(content={"error": "Invalid or missing JSON body"}, status_code=400)
 
