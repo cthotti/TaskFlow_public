a30ac574a6c5abcf1bde626ad1ccfe5f33bd80f8
cthotti <cthotti@gmail.com>
2025-08-20 22:18:47 +0530

    changing page.tsx

--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -1,14 +1,7 @@
 // src/app/notes/[id]/page.tsx
 import NoteEditor from "@/components/NoteEditor";
 
-interface NotePageProps {
-  params: {
-    id: string;
-  };
+export default function NotePage({ params }: { params: { id: string } }) {
+  return <NoteEditor noteId={params.id} />;
 }
 
-export default function NotePage({ params }: NotePageProps) {
-  const { id } = params;
-
-  return <NoteEditor noteId={id} />;
-}
